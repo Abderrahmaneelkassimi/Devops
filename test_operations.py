bfb0edede30e5e83051d4775df312b063106313b
elkassimiAbderrahmane <abderrahmaneelkassimi678@gmail.com>
2024-07-20 11:55:03 +0200

    commit

--- test_operations.py
+++ test_operations.py
@@ -6,7 +6,7 @@ print("Running test_add_operation_returns_correct_value")
 from operations import add
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
 
 if __name__ == "__main__":
     print("Running test_add_operation_returns_correct_value")
