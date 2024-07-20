0eebc4652e417ac9e4adfeee7a8658301bcc174d
elkassimiAbderrahmane <abderrahmaneelkassimi678@gmail.com>
2024-07-20 11:21:57 +0200

    finr

--- unit-test.py
+++ unit-test.py
@@ -1,25 +1,7 @@
-import unittest
-
-from operations import add, subtract, multiply, divide, power
 
 
-class TestSum(unittest.TestCase):
-
-    def test_add_operation_returns_correct_value(self):
-        self.assertEqual(add(10, 90), 110)
-	print("Running test_add_operation_returns_correct_value")
-    def test_substract_operation_returns_correct_value(self):
-        self.assertEqual(subtact(100, 90), 10)
-
-    def test_multiply_operation_returns_incorrect_value(self):
-        self.assertEqual(multiply(10, 10), 100)
-
-    def test_division_operation_returns_incorrect_value(self):
-        self.assertEqual(divide(100, 10), 10)
-
-    def test_division_operation_returns_error_when_divided_by_zero(self):
-        self.assertEqual(divide(10, 0), "Zero Division Error!")
+from ops import *
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add():
+   assert add(2,3)==5
