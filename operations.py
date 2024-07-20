bc9404878fe07c0658bdc1e32210b17cd5e55381
elkassimiAbderrahmane <abderrahmaneelkassimi678@gmail.com>
2024-07-20 17:01:05 +0200

    commit

--- operations.py
+++ operations.py
@@ -1,5 +1,15 @@
 def add(a, b):
     return a + b
 
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Zero Division Error!"
+    return x / y
 
 
--- test_operations.py
+++ test_operations.py
@@ -1,14 +1,28 @@
+import unittest
+from operations import add, subtract, multiply, divide
 
+class TestOperations(unittest.TestCase):
 
-from operations import *
-print("Running test_add_operation_returns_correct_value")
+    def test_add_operation_returns_correct_value(self):
+        print("Running test_add_operation_returns_correct_value")
+        self.assertEqual(add(10, 90), 100)
 
-from operations import add
+    def test_subtract_operation_returns_correct_value(self):
+        print("Running test_subtract_operation_returns_correct_value")
+        self.assertEqual(subtract(100, 90), 100)  # This should fail
 
-def test_add():
-    print("okokokok")
-    assert add(2, 3) == 5
+    def test_multiply_operation_returns_correct_value(self):
+        print("Running test_multiply_operation_returns_correct_value")
+        self.assertEqual(multiply(10, 10), 100)
 
-if __name__ == "__main__":
-    print("Running test_add_operation_returns_correct_value")
-    test_add()
+    def test_division_operation_returns_correct_value(self):
+        print("Running test_division_operation_returns_correct_value")
+        self.assertEqual(divide(100, 10), 10)
+
+    def test_division_operation_returns_error_when_divided_by_zero(self):
+        print("Running test_division_operation_returns_error_when_divided_by_zero")
+        self.assertEqual(divide(10, 0), "Zero Division Error!")
+
+if __name__ == '__main__':
+    unittest.main()
+
